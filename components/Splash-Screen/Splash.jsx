90529b3eb77704eeed314eff38a506142320eb2f
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2023-04-03 11:14:49 +0100

    Built the splash screen

---
+++ components/Splash-Screen/Splash.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { motion } from "framer-motion";
+import SplashText from "./SplashText";
+
+function Splash() {
+  return (
+    <motion.div
+      // animate={{
+      //   height: 0,
+      //   transition: { duration: 0.6, delay: 1.5 },
+      // }}
+      className="absolute z-50 flex items-center justify-center w-screen h-screen overflow-hidden bg-[#2A9988]"
+    >
+      <SplashText />
+    </motion.div>
+  );
+}
+
+export default Splash;
