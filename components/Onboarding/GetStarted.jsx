2669305abd341c39c0fb673d237737e37ac142a1
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2023-04-08 11:32:28 +0100

    Made some changes to the onboarding screen

--- components/Onboarding/GetStarted.jsx
+++ components/Onboarding/GetStarted.jsx
@@ -1,38 +1,49 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const GetStarted = () => {
   return (
-    <div className='GetStarted'>
-        <section className='image-section'></section>
-      <form>
-        <section className='rectangle'>
-          <div className='content'>
-            <div className='line'></div>
+    <div className="GetStarted">
+      <section className="hidden xl:block image-section"></section>
+      <form className="w-full xl:w-1/2">
+        <section className="rectangle">
+          <h2>Let&apos;s Get Started!</h2>
 
-            <div className='content-container'>
-              <section className='text'>
-                <h2>Let's Get Started!</h2>
-                <p>Enter your mobile number</p>
-              </section>
-              {<section className='input-section'>
-                <input type='tel' placeholder='+2348012345678'/>
-                {/* <div className='vectors'>
-                  <img src="\dropdown.png" alt="logo" className='dropdown'/>
-                  <img src="\Line 1.png" alt="logo" className='input-line'/>
-                </div> */}
-              </section>}
-              <section className='terms'>
-                <p>By continuing, you agree to our <span>Terms and conditions</span></p>
-              </section>
-              <div className='continue'>
-                <button>Continue</button>
-              </div>
-            </div>
-          </div>
+          <label>
+            First name
+            <input type="text" />
+          </label>
+
+          <label>
+            Last name
+            <input type="text" />
+          </label>
+
+          <label>
+            Phone no.
+            <input type="tel" placeholder="+2348012345678" />
+          </label>
+
+          <label>
+            Password
+            <input type="password" />
+          </label>
+
+          <p className="terms">
+            Already have an account?&nbsp;
+            <Link href={"/login"}>Login</Link>
+          </p>
+
+          <p className="terms">
+            By continuing, you agree to our&nbsp;
+            <span>Terms and conditions</span>
+          </p>
+
+          <button type="submit">Continue</button>
         </section>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default GetStarted;
